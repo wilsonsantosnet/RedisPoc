efa017217d04804bcf91a6e165669dce09c3aacd
Wilson Santos <wilsonsantosnet@gmail.com>
2020-11-08 14:29:31 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+
+namespace TesteRedis
+{
+    public class RedisStore
+    {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection;
+
+        static RedisStore()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+                IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            var configurationOptions = new ConfigurationOptions
+            {
+                EndPoints = { config["redis.connection"] },
+                AbortOnConnectFail = false,
+                
+                
+
+            };
+
+            LazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(configurationOptions));
+        }
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        public static IDatabase RedisCache => Connection.GetDatabase();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var redis = RedisStore.RedisCache;
+
+            if (redis.StringSet("testKey", "testValue"))
+            {
+                var val = redis.StringGet("testKey");
+
+                Console.WriteLine(val);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
